5b1fe77296b43485a5c6d209e436f3b6c1c82ceb
Amr7ahmed99 <amr7ahmed99@gmail.com>
2023-04-25 23:46:57 +0200

    Query all posts and find post by id

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,32 +2,32 @@
 
 namespace App\Http\Controllers;//Composer using the namespace to autoload the required files using "psr-4"
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-
-    
-
-    
-
+    private $posts;
 
     public function index(){
-        $posts= [
-            ["id"=> 1, "title"=> "Laravel", "posted_by"=> "Amr", "created_at"=> "2023-4-24"],
-            ["id"=> 2, "title"=> "JS", "posted_by"=> "Ahmed", "created_at"=> "2023-4-22"],
-            ["id"=> 3, "title"=> "Node", "posted_by"=> "Saleh", "created_at"=> "2023-4-20"],
-        ];
+        // $posts= [
+        //     ["id"=> 1, "title"=> "Laravel", "posted_by"=> "Amr", "created_at"=> "2023-4-24"],
+        //     ["id"=> 2, "title"=> "JS", "posted_by"=> "Ahmed", "created_at"=> "2023-4-22"],
+        //     ["id"=> 3, "title"=> "Node", "posted_by"=> "Saleh", "created_at"=> "2023-4-20"],
+        // ];
+        $this->posts= Post::all();//returns eloquent collection object
         return view('posts.index', [
-            "posts"=> $posts,
+            "posts"=> $this->posts,
             "pageName"=> "Laravel-Blogs"
         ]);
     }
 
     public function show($id){
-
+        $post= Post::find($id);
+        // $post= Post::where('id', $id)->get();//get all records with this value
+        // $post= Post::where('id', $id)->first();//first = limit 1
         return view('posts.show', [
-            'post'=> $this->fetchPostByID($id),
+            'post'=> $post,
             'pageName'=> 'Laravel-Blog'
         ]);
     }

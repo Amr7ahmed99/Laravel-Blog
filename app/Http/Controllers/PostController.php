97bf668c9698dba4e46203e0f703ee26e906d1c0
Amr7ahmed99 <amr7ahmed99@gmail.com>
2023-04-25 23:01:14 +0200

    Add fetchPostById function to PostController

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,7 +6,12 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+
+    
+
+    
+
+
     public function index(){
         $posts= [
             ["id"=> 1, "title"=> "Laravel", "posted_by"=> "Amr", "created_at"=> "2023-4-24"],
@@ -20,22 +25,9 @@ class PostController extends Controller
     }
 
     public function show($id){
-        //Dummy Data
-        $posts= [
-            ["id"=> 1, "title"=> "Laravel", "description"=> "this is a post description","posted_by"=> "Amr", "created_at"=> "2023-4-24"],
-            ["id"=> 2, "title"=> "JS", "description"=> "this is a post description", "posted_by"=> "Ahmed", "created_at"=> "2023-4-22"],
-            ["id"=> 3, "title"=> "Node", "description"=> "this is a post description", "posted_by"=> "Saleh", "created_at"=> "2023-4-20"],
-        ];
 
-        $res= [];
-        foreach($posts as $post){
-            if($post['id'] == $id){
-                $res= $post;
-                break;
-            }
-        }
         return view('posts.show', [
-            'post'=> $res,
+            'post'=> $this->fetchPostByID($id),
             'pageName'=> 'Laravel-Blog'
         ]);
     }
@@ -49,14 +41,31 @@ class PostController extends Controller
     }
 
     public function edit($id){
+        
         return view('posts.edit', [
-
+            "post"=> $this->fetchPostByID($id),
             "pageName"=> "Laravel-Edit-Blog"
         ]);
     }
     
     public function destroy($id){
         return "Delete Page";
+    }
+
+    private function fetchPostByID($Id){
+        $posts= [
+            ["id"=> 1, "title"=> "Laravel", "description"=> "this is a post description", "posted_by"=> "Amr", "created_at"=> "2023-4-24"],
+            ["id"=> 2, "title"=> "JS", "description"=> "this is a post description", "posted_by"=> "Ahmed", "created_at"=> "2023-4-22"],
+            ["id"=> 3, "title"=> "Node", "description"=> "this is a post description", "posted_by"=> "Saleh", "created_at"=> "2023-4-20"],
+        ];
+        $res= [];
+        foreach($posts as $post){
+            if($post['id'] == $Id){
+                $res= $post;
+                break;
+            }
+        }
+        return $res;
     }
 
 }
--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -7,15 +7,15 @@
                     @csrf
                     <div class="mb-3">
                         <label for="title" class="form-label">Title </label>
-                        <input type="text" class="form-control" id="title" name="title">
+                        <input type="text" class="form-control" id="title" name="title" value="{{$post['title']}}">
                     </div>
                     <div class="mb-3">
                         <label for="description" class="form-label">Description</label>
-                        <textarea class="form-control" id="description" cols="30" rows="5" name="description"></textarea>
+                        <textarea class="form-control" id="description" cols="30" rows="5" name="description" >{{$post['description']}}</textarea>
                     </div>
                     <div class="mb-3">
                         <label for="creator" class="form-label">Creator </label>
-                        <input type="text" class="form-control" id="creator" name= "creator">
+                        <input type="text" class="form-control" id="creator" name= "creator" value="{{$post['posted_by']}}">
                     </div>
                     <button type="submit" class="btn btn-success">Create</button>
                    

33209ae82d394d11d571734563056dc3f0966645
Amr7ahmed99 <amr7ahmed99@gmail.com>
2023-04-29 02:04:53 +0200

    Add CRUD operations

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;//Composer using the namespace to autoload the required files using "psr-4"
 
 use App\Models\Post;
+use Exception;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -10,11 +11,6 @@ class PostController extends Controller
     private $posts;
 
     public function index(){
-        // $posts= [
-        //     ["id"=> 1, "title"=> "Laravel", "posted_by"=> "Amr", "created_at"=> "2023-4-24"],
-        //     ["id"=> 2, "title"=> "JS", "posted_by"=> "Ahmed", "created_at"=> "2023-4-22"],
-        //     ["id"=> 3, "title"=> "Node", "posted_by"=> "Saleh", "created_at"=> "2023-4-20"],
-        // ];
         $this->posts= Post::all();//returns eloquent collection object
         return view('posts.index', [
             "posts"=> $this->posts,
@@ -24,7 +20,7 @@ class PostController extends Controller
 
     public function show($id){
         $post= Post::find($id);
-        // $post= Post::where('id', $id)->get();//get all records with this value
+        // $post= Post::where('id', $id)->get();//get all records with common value
         // $post= Post::where('id', $id)->first();//first = limit 1
         return view('posts.show', [
             'post'=> $post,
@@ -32,24 +28,43 @@ class PostController extends Controller
         ]);
     }
 
-    public function create(Request $request){
-        // dd($request->request);
-        // return "created";
-       
-        return redirect('posts');
+    public function create(){
+        return view("posts.create", [
+            "pageName"=> "Laravel-Create-Blogs"
+        ]);
         //  return $this->index()->with('status', 'Blog Post Form Data Has Been inserted');
     }
 
-    public function edit($id){
+    public function store(Request $myRequestObj){//Hinting Type
+        $data= $myRequestObj->all();//Get Request Data
+        // Post::create([
+        //     'title'=> $data['title'],
+        //     "description"=> $data['description'],
+        // ]);
+        //Insert New Data Into Posts Table
+        Post::create($data);//Takes [title, description] from data object according to $fillable
+        return redirect()->route('posts.index');
+    }
+
+    public function edit($post){
         
         return view('posts.edit', [
-            "post"=> $this->fetchPostByID($id),
+            "post"=> Post::find($post),
             "pageName"=> "Laravel-Edit-Blog"
         ]);
     }
+
+    public function update(Request $myRequestObj, $id){
+        $data= $myRequestObj->all();
+        $updateFlag= Post::find($id)->update(['title'=> $data['title'], 'description'=> $data['description']]);
+        // dd($updateFlag);
+        return redirect()->route('posts.index');
+    }
     
     public function destroy($id){
-        return "Delete Page";
+        Post::find($id)->delete();
+        return redirect()->route('posts.index');
+
     }
 
     private function fetchPostByID($Id){
